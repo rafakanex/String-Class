cc4fa2b3cfce03d5d797c4417984331cfc0cf24f
Lann <rafakanex@gmail.com>
2018-10-03 14:01:01 +0200

    class updateds

--- String Class/Main.cpp
+++ String Class/Main.cpp
@@ -7,8 +7,10 @@ int main()
 	String str2(str1);
 	String str3 = "Wuuba lubba dub dub!";
 	String str4 = str3;
+	String str5;
 
-	if (str1 == str2 && str2 == str3 && str3 == str4)printf("Wuuba lubba dub dub!\n");
+	if (str1 == str2 && str2 == str3 && str3 == str4 && str5 == "")printf("Wuuba lubba dub dub!\n");
+
 
 	system("pause");
 	return 0;
--- String Class/String.h
+++ String Class/String.h
@@ -2,50 +2,68 @@
 #define _STRING_H__
 
 #include <string.h>
+#include <assert.h>
 
 class String
 {
 private:
-	char* str;
-	unsigned int size;
+	char*			str;
+	unsigned int	size;
 
 public:
 	String()
 	{
-		str = new char[size];
-		str[0] = '\0'; 
+		str = new char[size = 1];
+		str[0] = '\0'; // str = ""
+					   //memset(str, 0, sizeof(*str)); // str = ""
 	}
 
 	String(const char* string)
 	{
-		size = strlen(string) + 1;
-		str = new char[size];
-		strcpy_s(str, size, string);
+		assert(string != nullptr);
+		if (string != nullptr)
+		{
+			size = strlen(string) + 1;
+			str = new char[size];
+			strcpy_s(str, size, string);
+		}
 	}
 
 	String(const String& string)
 	{
-		size = string.size;
-		str = new char[size];
-		strcpy_s(str, size, string.str);
+		assert(string.str != nullptr);
+		if (string.str != nullptr)
+		{
+			size = string.size;
+			str = new char[size];
+			strcpy_s(str, size, string.str);
+		}
 	}
 
 	~String()
 	{
-		delete[] str; // cannot be nullptr cause of the default constructor -> Clear()
+		assert(str != nullptr);
+		if (str != nullptr)
+		{
+			delete[] str;
+			str = new char[size = 1];
+			str[0] = '\0';
+		}
 	}
 
 	String operator = (const char* string)
 	{
+		assert(str != nullptr);
 		if (string != nullptr)
 		{
-			if (strlen(string) + 1 > size) // it's better having more unsued char allocated than deleting and allocating again
+			if (strlen(string) + 1 > size)
 			{
+				delete[] str;
 				size = strlen(string) + 1;
 				str = new char[size];
 			}
 			else
-				str[0] = '\0';
+				str[0] = '\0'; // it's better having unsued chars allocated than deleting and allocating again
 
 			strcpy_s(str, size, string);
 		}
@@ -59,16 +77,6 @@ public:
 	{
 		return strcmp(str, string.str) == 0;
 	}
-
-	/*void Alloc(const unsigned int memory) 
-	{
-		str = new char[size];
-	}
-
-	void Clear() const
-	{
-		str[0] = '\0'; // or memset
-	}*/
 };
 
 #endif // !_STRING_H__
